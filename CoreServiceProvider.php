4e559cd245fb8295526ffa549c057d8a125db610
Bartvvenrooij <bartvvenrooij@gmail.com>
2018-10-26 08:58:37 +0200

    New awesome

--- CoreServiceProvider.php
+++ CoreServiceProvider.php
@@ -1,6 +1,17 @@
 <?php
 
-class CoreServiceProvider
+namespace App\Modules\Core;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
 {
 
+    /**
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+    }
 }
